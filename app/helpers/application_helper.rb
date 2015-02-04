aedef2a0e05f8e1c5beb356f1cb5334990dd6eb9
greattambino <marctambara@gmail.com>
2015-02-04 00:35:48 -0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns full page title on a per-page basis. 
+  def full_title(page_title = '')
+    base_title = "MusicalRec"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
